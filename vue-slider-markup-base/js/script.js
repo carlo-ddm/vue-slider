eeabc700f6930feec6cb6cc018d051be0c8dd1ed
carlo-ddm <carloduminuco90@gmail.com>
2022-05-03 15:33:55 +0200

    primo step --> dinamicità ad immagine principale cliccando ..

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -1,35 +1,59 @@
-// Descrizione:
-// Partendo dal markup consegnate rendere dinamici tutti i contenuti (foto e testi) e al click su una thumb, visualizzare in grande l’immagine corrispondente
-// Bonus:
-// 1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-// 2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+// 1) Ciclare gli oggetti dell'array slide
+// 2) Ciclare le proprietà del singolo oggetto
 
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+const app = new Vue({
+    el: '#app',
+    data: {            //data è oggetto che ha propr che è array di ogg
+        slides : [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
 
-console.log(slides);
+        counterObject: 0,
+    },
+
+    methods: {
+        nextObject(){
+            this.counterObject ++;
+            if(this.counterObject > this.slides.length - 1){
+                this.counterObject = 0;
+            }
+            console.log('Constrollo NEXT --> ',this.counterObject);
+        },
+        
+        prevObject(){
+            this.counterObject --;
+            if(this.counterObject < 0){
+                this.counterObject = this.slides.length - 1
+            }
+            console.log('Controllo PREV --> ',this.counterObject);
+        },
+    },
+})
+
+
+
+// console.log(slides);
